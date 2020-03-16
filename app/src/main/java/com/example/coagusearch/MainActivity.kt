e2cec8ad507217169d67518bde0b3c6dcff9d0e1
msalel <msalel15@ku.edu.tr>
2020-03-17 02:50:48 +0300

    Lottie anim added

--- app/src/main/java/com/example/coagusearch/MainActivity.kt
+++ app/src/main/java/com/example/coagusearch/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.example.coagusearch
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.StrictMode
 import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coagusearch.network.Auth.model.AuthRepository
 import com.example.coagusearch.network.Interceptors.AuthInterceptor
-import com.example.coagusearch.network.Users.model.UsersRepository
-import com.example.coagusearch.network.Users.response.UserResponse
 import com.example.coagusearch.network.shared.RetrofitClient
 import com.example.coagusearch.network.shared.appModule
+import kotlinx.android.synthetic.main.loading_lottie_dialog.*
 import kotlinx.android.synthetic.main.loginscreen.*
-import okhttp3.OkHttpClient
-import okhttp3.Protocol
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.android.startKoin
+import java.util.concurrent.TimeUnit
 
 
-class MainActivity: AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     private val authInterceptor: AuthInterceptor by inject()
     private val retrofitClient: RetrofitClient by inject()
 
@@ -32,25 +27,30 @@ class MainActivity: AppCompatActivity(){
         startKoin(this, listOf(appModule))
 
         // Force init AuthRepository
-        AuthRepository(applicationContext, authInterceptor,retrofitClient)
+        AuthRepository(applicationContext, authInterceptor, retrofitClient)
 
-        var authRepository:AuthRepository = get()
+        var authRepository: AuthRepository = get()
 
 
         setContentView(R.layout.loginscreen)
 
-        LoginButton.setOnClickListener { if (!isPasswordValid(PasswordInput.text!!)) {
-            PasswordInput.error="Password is not valid"
-        } else {
-            // Clear the error.
-            val response = authRepository.signIn("14051222123","123456")
-            println(response?.accessToken)
+        LoginButton.setOnClickListener {
+            if (!isPasswordValid(PasswordInput.text!!)) {
+                PasswordInput.error = "Password is not valid"
+            } else {
+                // Clear the error.MainActivity.this
+                setContentView(R.layout.loading_lottie_dialog)
+                lottieAnimationView.playAnimation()
 
-            val intent = Intent(this,main::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                var response = authRepository.signIn("14051222123", "123456")
 
-        } }
+
+                //val intent = Intent(this,main::class.java)
+                //startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+            }
+        }
         PasswordInput.setOnKeyListener { _, _, _ ->
             if (isPasswordValid(PasswordInput.text!!)) {
                 PasswordInput.error = null
--- app/src/main/java/com/example/coagusearch/network/Auth/model/AuthRepository.kt
+++ app/src/main/java/com/example/coagusearch/network/Auth/model/AuthRepository.kt
@@ -1,11 +1,12 @@
 package com.example.coagusearch.network.Auth.model
 
 import android.content.Context
-import androidx.core.content.edit
+import android.content.DialogInterface
+import android.graphics.Color
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.coagusearch.Constants
-import com.example.coagusearch.MainActivity
 import com.example.coagusearch.R
-import com.example.coagusearch.network.ApiResult
 import com.example.coagusearch.network.Auth.request.LoginRequest
 import com.example.coagusearch.network.Auth.request.RefreshRequest
 import com.example.coagusearch.network.Auth.request.SignUpRequest
@@ -15,16 +16,14 @@ import com.example.coagusearch.network.Auth.response.SignUpResponse
 import com.example.coagusearch.network.Interceptors.AuthInterceptor
 import com.example.coagusearch.network.shared.RetrofitClient
 import com.example.coagusearch.network.shared.response.ApiResponse
-import com.example.coagusearch.typing.Ok
-import com.example.coagusearch.typing.single
 import com.example.coagusearch.utils.PreferenceHelper
 import com.example.coagusearch.utils.securelyGetString
 import com.example.coagusearch.utils.securelyPutString
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.loginscreen.*
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.Callback
+import retrofit2.Response
+
 
 class AuthRepository(
     private val context: Context,
@@ -49,20 +48,66 @@ class AuthRepository(
                     call: Call<LoginResponse>,
                     response: Response<LoginResponse>
                 ) {
-                    val auth = response.body() as LoginResponse
-                    val sharedPref = PreferenceHelper.defaultPrefs(context)
+                    if (response.isSuccessful && response.body() is LoginResponse) {
+                        val auth = response.body() as LoginResponse
+                        val sharedPref = PreferenceHelper.defaultPrefs(context)
 
-                    sharedPref.securelyPutString(context, Constants.SHARED_PREF_ACCESS_TOKEN, auth.accessToken
-                        ?: "")
-                    sharedPref.securelyPutString(context, Constants.SHARED_PREF_REFRESH_TOKEN, auth.refreshToken
-                        ?: "")
-                    sharedPref.securelyPutString(context, Constants.SHARED_PREF_TOKEN_TYPE, auth.tokenType
-                        ?: "")
-                    //(context as MainActivity).LoginButton.text =
-                     //   response.body()?.tokenType.toString()
-                    loginResponse = response.body()
-                    println(sharedPref.securelyGetString(context, Constants.SHARED_PREF_ACCESS_TOKEN))
-                    println(sharedPref.securelyGetString(context, Constants.SHARED_PREF_REFRESH_TOKEN))
+                        sharedPref.securelyPutString(
+                            context, Constants.SHARED_PREF_ACCESS_TOKEN, auth.accessToken
+                                ?: ""
+                        )
+                        sharedPref.securelyPutString(
+                            context, Constants.SHARED_PREF_REFRESH_TOKEN, auth.refreshToken
+                                ?: ""
+                        )
+                        sharedPref.securelyPutString(
+                            context, Constants.SHARED_PREF_TOKEN_TYPE, auth.tokenType
+                                ?: ""
+                        )
+                        //(context as MainActivity).LoginButton.text =
+                        //   response.body()?.tokenType.toString()
+                        loginResponse = response.body()
+                        println(
+                            sharedPref.securelyGetString(
+                                context,
+                                Constants.SHARED_PREF_ACCESS_TOKEN
+                            )
+                        )
+                        println(
+                            sharedPref.securelyGetString(
+                                context,
+                                Constants.SHARED_PREF_REFRESH_TOKEN
+                            )
+                        )
+                    }else{
+                        val errorResponse =
+                            Gson().fromJson<ApiResponse>(response.errorBody()?.string(), ApiResponse::class.java)?.message
+                                ?: context.resources.getString(R.string.errorOccurred)
+
+                        val builder = AlertDialog.Builder(context)
+                        // Set the alert dialog title
+                        builder.setTitle("App background color")
+                        println(response.body())
+                        // Display a message on alert dialog
+                        builder.setMessage(errorResponse)
+                        // Set a positive button and its click listener on alert dialog
+                        builder.setPositiveButton("YES"){dialog, which ->
+                            // Do something when user press the positive button
+                            Toast.makeText(context,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+                        }
+                        // Display a negative button on alert dialog
+                        builder.setNegativeButton("No"){dialog,which ->
+                            Toast.makeText(context,"You are not agree.",Toast.LENGTH_SHORT).show()
+                        }
+                        // Display a neutral button on alert dialog
+                        builder.setNeutralButton("Cancel"){_,_ ->
+                            Toast.makeText(context,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+                        }
+                        // Finally, make the alert dialog using builder
+                        val dialog: AlertDialog = builder.create()
+                        // Display the alert dialog on app interface
+                        dialog.show()
+                    }
                 }
 
             })
